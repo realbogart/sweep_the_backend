e148d96cf3d3f2e7228e291790b274d4b0578ec2
Johan Yngman <johan.yngman@paradoxinteractive.com>
2023-09-29 15:48:38 +0200

    Parsing config file

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,8 @@ import Servant.API
 import GHC.Generics
 import Network.Wai.Handler.Warp
 import Data.Aeson
+import Data.ByteString.Lazy.Char8 qualified as BS
+import Control.Exception
 
 type ScheduleAPI = "getSchedule" :> Get '[JSON] [Schedule]
 
@@ -21,7 +23,19 @@ data Schedule = Schedule
   , playlistLink :: String
   } deriving (Show, Eq, Generic)
 
+data Config = Config
+  { websocket_server_ip :: String
+  , httpserver_port :: Int
+  , websocket_port :: Int
+  , stream_ip :: String
+  , password :: String
+  , default_background :: String
+  } deriving (Show, Generic)
+
 instance ToJSON Schedule
+
+instance ToJSON Config
+instance FromJSON Config
 
 testSchedules = [Schedule "hello" "now" "johan" "https://bild" "https://spotify-link"]
 
@@ -34,6 +48,27 @@ testServer = return testSchedules
 sweepTheBackend :: Application
 sweepTheBackend = serve scheduleAPI testServer
 
+readConfig :: FilePath -> IO (Maybe Config)
+readConfig fileName = do
+  result <- try (BS.readFile fileName) :: IO (Either IOException BS.ByteString)
+  case result of
+    Left ex -> do 
+      putStrLn $ "Unable to read config file " ++ show ex 
+      return Nothing
+    Right content -> do 
+      let parseResult = decode content
+      case parseResult of
+        Nothing -> do 
+          putStrLn $ "Could not parse JSON from " ++ show fileName
+          return Nothing
+        Just config -> return $ Just config
+
 main :: IO ()
-main = run 1337 sweepTheBackend
+main = do 
+  result <- readConfig "config.json"
+  case result of
+    Nothing -> putStrLn "Invalid configuration"
+    Just config -> print config
+  run 1337 sweepTheBackend
+  return ()
 
