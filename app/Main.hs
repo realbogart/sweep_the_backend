d39473874318e03481faed60a7e055e3d775ff3d
Johan Yngman <johan.yngman@gmail.com>
2023-10-02 17:42:21 +0200

    Websockets initial implementation

--- app/Main.hs
+++ app/Main.hs
@@ -15,7 +15,10 @@ import Network.HTTP.Types
 import Data.Aeson
 import Control.Concurrent.MVar
 import Control.Monad.IO.Class (MonadIO(liftIO))
+import Control.Monad
+import Control.Exception qualified as CE
 import Data.ByteString.Char8 qualified as BS
+import Data.Char
 import Data.List qualified as L
 import Data.Text qualified as T
 import Data.Text.Lazy qualified as TL
@@ -24,6 +27,60 @@ import Data.Text.Lazy.Encoding qualified as TLE
 import Data.Text.IO qualified as TIO
 import Data.Text.Lazy.IO qualified as TLIO
 import System.FilePath.Posix
+import Control.Concurrent.Async qualified as A
+import qualified Network.WebSockets as WS
+
+type Client = (T.Text, WS.Connection)
+type ServerState = [Client]
+
+addClient :: Client -> ServerState -> ServerState
+addClient c state = c : state
+
+removeClient :: Client -> ServerState -> ServerState
+removeClient c = filter ((== fst c) . fst) 
+
+numClients :: ServerState -> Int
+numClients = length
+
+clientExists :: Client -> ServerState -> Bool
+clientExists client = any ((== fst client) . fst)
+
+broadcast :: T.Text -> ServerState -> IO ()
+broadcast message clients = do
+  putStrLn $ "Broadcasting to " ++ show (length clients) ++ " clients:"
+  TIO.putStrLn message
+  forM_ clients $ \(_, conn) -> WS.sendTextData conn message
+
+talk :: Client -> MVar ServerState -> IO ()
+talk (user, conn) state = forever $ do
+  msg <- WS.receiveData conn
+  readMVar state >>= broadcast msg
+
+wsApplication :: MVar ServerState -> WS.ServerApp
+wsApplication state pending = do
+  conn <- WS.acceptRequest pending
+  WS.withPingThread conn 30 (return ()) $ do
+    msg :: T.Text <- WS.receiveData conn
+    clients <- readMVar state
+    case msg of
+      -- _ | any ($ fst client)  [T.null, T.any isSpace] ->
+      --       WS.sendTextData conn ("Name cannot contain whitespace, and cannot be empty" :: T.Text)
+        -- | clientExists client clients ->
+        --     WS.sendTextData conn ("Client already exists" :: T.Text)
+      _ | otherwise -> 
+            flip CE.finally disconnect $ do
+              modifyMVar_ state $ \s -> return $ addClient client s
+                -- let s' = addClient client s
+                -- WS.sendTextData conn $ "Welcome! Users: " <> T.intercalate ", " (map fst s)
+                -- broadcast (fst client <> " joined") s'
+                -- return s'
+              talk client state
+        where client = ("none", conn)
+              disconnect = do
+                modifyMVar_ state $ \s -> return $ removeClient client s
+                --   let s' = removeClient client s in return (s', s')
+                -- return ()
+                -- broadcast (fst client <> " disconnected") s
 
 data Slot = Schedule
   { name :: T.Text 
@@ -104,10 +161,15 @@ main = do
   result <- readConfig configFile
   case result of
     Left err -> putStrLn ("Invalid configuration: " ++ err)
-    Right config -> do  let port = httpserver_port config
+    Right config -> do  let http_port = httpserver_port config
+                        let ws_port = websocket_port config
                         putStrLn $ "Successfully loaded '" ++ configFile ++ "'."
-                        putStrLn $ "Starting HTTP server on port " ++ show port ++ "."
+                        putStrLn $ "Starting HTTP server on port " ++ show http_port ++ "."
+                        putStrLn $ "Starting websocket server on port " ++ show ws_port ++ "."
                         sweepState <- newMVar $ SweepState{schedule = testSchedule, currentSlot = - 1}
-                        run port (sweepTheApplication sweepState config)
+                        wsState <- newMVar []
+                        servantAsync <- A.async $ run http_port (sweepTheApplication sweepState config)
+                        websocketAsync <- A.async $ WS.runServer "127.0.0.1" ws_port (wsApplication wsState)
+                        A.waitBoth servantAsync websocketAsync
                         return ()
 
